5d033f92a52fbc5a58bfb3c3c8cc1227757d7b4d
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2021-11-13 13:52:08 -0500

    Working on getUsers function. Added constants and EventListener partial code.

---
+++ js/main.js
@@ -0,0 +1,40 @@
+//Constants:
+
+const api_url = 'https://jsonplaceholder.typicode.com/users'
+
+// Cached Element References:
+
+const userID = document.getElementById("newNote");
+
+// Event Listeners:
+
+// userID.addEventListener('click', function() {
+//     displayPosts();
+// })
+
+// Functions:
+
+// Function 01: Fetch JSON from API for table.
+
+async function getUsers() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    console.log(data);
+}
+
+getUsers();
+
+// Function 02: Display posts after user clicks on "User".
+
+// fetch('https://jsonplaceholder.typicode.com/users', {
+//     method: 'GET',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//         // id:'',
+//         name:'Leanne Graham'
+//     })
+// })
+//     .then(response => response.json())
+//     .then(json => console.log(json))
