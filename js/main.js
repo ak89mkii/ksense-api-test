d5dd472535e2aa82679078f01249cf132d331863
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2021-11-14 01:20:12 -0500

    API posts renders for udser ID 1.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,7 @@
 //Constants:
 
 const api_url = 'https://jsonplaceholder.typicode.com/users'
-
-// Cached Element References:
-
-const userID = document.getElementById("user");
-
-// Event Listeners:
-
-// userID.addEventListener('click', function() {
-//     displayPosts();
-// })
+const api_url_post = 'https://jsonplaceholder.typicode.com/posts?userId=1'
 
 // Functions:
 
@@ -21,6 +12,10 @@ async function getUsers() {
     const data = await response.json();
     console.log(data)
 
+    const responseAlt = await fetch(api_url_post);
+    const dataAlt = await responseAlt.json();
+    console.log(dataAlt)
+
     buildTable(data);
 
     function buildTable(data) {
@@ -29,20 +24,47 @@ async function getUsers() {
         for (let i = 0; i < data.length; i++) {
             let row =
                 `<tr>
-                    <td>${data[i].id}</td>
-                    <td>${data[i].name}</td>
+                    <td class='halo' id='${data[i].id}'>${data[i].id}</td>
+                    <td class='halo' id='${data[i].name}'>${data[i].name}</td>
                 </tr>`
-                table.innerHTML += row
+            table.innerHTML += row
         }
     }
 
-    // document.getElementById('userID').textContent = id;
-    // document.getElementById('userName').textContent = name;
+    // Cached Element References:
 
+    let userID01 = document.getElementById('Leanne Graham');
+    let userID02 = document.getElementById('Ervin Howell');
+    let userID03 = document.getElementById('Clementine Bauch');
+    let userID04 = document.getElementById('Patricia Lebsack');
+    let userID05 = document.getElementById('Chelsey Dietrich');
+    let userID06 = document.getElementById('Mrs. Dennis Schulist');
+    let userID07 = document.getElementById('Kurtis Weissnat');
+    let userID08 = document.getElementById('Nicholas Runolfsdottir V');
+    let userID09 = document.getElementById('Glenna Reichert');
+    let userID10 = document.getElementById('Clementina DuBuque');
 
+    // Event Listeners:
     
-    // console.log(data.id);
-    // console.log(data.name);
+    userID01.addEventListener('click', function() {
+        displayPosts(dataAlt);
+    });
+
+    function displayPosts(dataAlt) {
+        let name = document.getElementById('posterName')
+
+        for (let i = 0; i < dataAlt.length; i++) {
+            let goku = ''
+            name.innerHTML = goku
+        }
+        
+        for (let i = 0; i < dataAlt.length; i++) {
+            let goku =
+                `<div class="alert alert-dark" role="alert"><h5>${dataAlt[i].title}</h5>
+                <p>${dataAlt[i].body}</p></div>`
+            name.innerHTML += goku
+        }
+    }
 }
 
 getUsers();
